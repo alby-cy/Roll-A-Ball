6cdfffc00762b5acb103dcd39e34eefa7d7eb835
alby-cy <droper1@myjmc.edu.au>
2024-03-21 11:45:35 +1100

    Scene & Lighting
    Added level piece prefabs, added skybox lighting

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
